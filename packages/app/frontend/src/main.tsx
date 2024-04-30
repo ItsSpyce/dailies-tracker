6c2ea47edd31fd20f8bda234977d27c1e9768bb7
amukasekon <168530035+amukasekon@users.noreply.github.com>
2024-04-30 14:36:20 +0200

    Update main.tsx

--- packages/app/frontend/src/main.tsx
+++ packages/app/frontend/src/main.tsx
@@ -20,7 +20,7 @@ const localeSwitch = (lang: string) =>
     .with('en-US.UTF-8', () => locales.enUS)
     .with('de-DE.UTF-8', () => locales.deDE)
     .with('vi-VN.UTF-8', () => locales.viVN)
-    .with('ES-SPA.UTF-8', () => locales.viVN)
+    .with('es-ES.UTF-8', () => locales.esES)
     // insert more locales here
     .otherwise(() => locales.enUS);
 
